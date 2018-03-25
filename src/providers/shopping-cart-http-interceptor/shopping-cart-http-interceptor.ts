6f8625348ad3e6e2cb440e1321a8fc7b5c75a1d8
Emmanuel <ogoma.emmanuel@gmail.com>
2018-03-25 17:15:04 +0300

    Added Bearer Authentication Header int Htpp interceptor, All http request now have bearer tokens

--- src/providers/shopping-cart-http-interceptor/shopping-cart-http-interceptor.ts
+++ src/providers/shopping-cart-http-interceptor/shopping-cart-http-interceptor.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpSentEvent, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpUserEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import{Storage} from '@ionic/storage';
+import 'rxjs/add/operator/mergeMap';
+import { User } from '@firebase/auth-types';
 
 /*
   Generated class for the ShoppingCartHttpInterceptorProvider provider.
@@ -10,23 +13,33 @@ import { Observable } from 'rxjs/Observable';
 */
 @Injectable()
 export class ShoppingCartHttpInterceptorProvider implements HttpInterceptor  {
-  constructor() {
+  constructor(private storage:Storage) {
     console.log('Hello ShoppingCartHttpInterceptorProvider Provider');
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
-   if (req.responseType == 'json') {
-			req = req.clone({ responseType: 'text' });
+  
+  
+
+   return Observable.fromPromise(this.storage.get("loggedInUserDetails")).mergeMap((userDetails:any)=>{
+     console.log("User at Interceptor",userDetails);
+     if (req.responseType == 'json') {
+      req = req.clone({ responseType: 'text',
+      setHeaders: {  
+        Authorization: "Bearer "+JSON.parse(userDetails).stsTokenManager.accessToken 
+       }  
+     });
 
 			return next.handle(req).map(response => {
 				if (response instanceof HttpResponse) {
 					response = response.clone<any>({ body: JSON.parse(response.body) });
 				}
-
 				return response;
 			});
 		}
 
 		return next.handle(req);
+    })
+   
   }
 }
